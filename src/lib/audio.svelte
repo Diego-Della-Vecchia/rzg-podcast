97bb43756fe45eeb44347caa3eae3a7da6f65b75
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-08-31 23:40:01 +0200

    add slider to control time

--- src/lib/audio.svelte
+++ src/lib/audio.svelte
@@ -5,7 +5,9 @@
 
 	let paused = $state(false);
 
-	let audioElement: HTMLAudioElement;
+	let audioElement: HTMLAudioElement = $state({} as HTMLAudioElement);
+
+	let duration = $state(0);
 
 	function play() {
 		if (paused) {
@@ -15,9 +17,15 @@
 		}
 		paused = !paused;
 	}
+
+	$effect(() => {
+		if (current.time == audioElement.duration) {
+			current.time = 0;
+		}
+	});
 </script>
 
-<audio bind:this={audioElement} src={url} bind:paused bind:currentTime={current.time}></audio>
+<audio bind:this={audioElement} src={url} bind:paused bind:currentTime={current.time} bind:duration></audio>
 
 <div class="container">
 	{#if paused}
@@ -29,6 +37,17 @@
 			<img src="/pause.svg" alt="" />
 		</button>
 	{/if}
+	<input
+		type="range"
+		bind:value={current.time}
+		onchange={() => {
+			audioElement.play();
+			paused = false;
+		}}
+		min={0}
+		max={duration}
+		step={0.1}
+	/>
 </div>
 
 <style>
@@ -42,6 +61,7 @@
 		width: min(80vw, 400px);
 		border-radius: 50px;
 		margin: 0 auto;
+		gap: 20px;
 	}
 	button {
 		width: 60px;
@@ -50,6 +70,9 @@
 		transition: border-radius 0.2s ease-in-out;
 		cursor: pointer;
 		border: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
 	.play {
@@ -77,5 +100,32 @@
 	}
 	img {
 		width: 30px;
+	}
+
+	input {
+		width: 50%;
+		-webkit-appearance: none;
+		appearance: none;
+		-moz-appearance: none;
+		cursor: pointer;
+		background-size: contain;
+	}
+
+	input::-webkit-slider-runnable-track {
+		-webkit-appearance: none;
+		appearance: none;
+		height: 15px;
+		background: var(--grey);
+		border-radius: 10px;
+	}
+
+	input::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		width: 20px;
+		height: 30px;
+		background: var(--color);
+		border-radius: 5px;
+		margin-top: -7.5px; /* Half the difference between thumb height and track height */
 	}
 </style>
